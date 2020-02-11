192ef3afe30932e6b6e24b86e139cf25596bc6a3
Gene Grady <grady.gene.thomas@gmail.com>
2020-02-11 11:44:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function findMinAndRemove(array){
-
+    let min = array[0];
+    let minI = 0;
+    for(let i = 0; i < array.length; i++){
+        let cE = array[i];
+        if(cE < min){
+            min = cE;
+            minI = i;
+        }
+    }
+    array.splice(minI, 1)
+    return min
 }
 
 function insertionSort(array){
-
+    let newMin;
+    let sorted = []
+    while(array.length != 0){
+        newMin = findMinAndRemove(array)
+        sorted.push(newMin)
+    }
+    return sorted;
 }
